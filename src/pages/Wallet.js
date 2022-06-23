2ae0e2f9420e536b2f19e37df647a1b39ccc5684
Paulo de Tarso <paulo.oinab@gmail.com>
2022-06-23 17:23:10 -0300

    7. Desenvolva uma tabela com os gastos contendo as seguintes características

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -130,6 +130,21 @@ class Wallet extends React.Component {
           </label>
           <button type="button" onClick={ this.handleClick }>Adicionar despesa</button>
         </section>
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+        </table>
       </>
     );
   }
